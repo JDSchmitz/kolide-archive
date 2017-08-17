3223e798d11145c0688f6f0cecf7bdade469c829
JDSchmitz <jdschmit@cord.edu>
2017-08-16 23:02:31 -0400

    Update node.go

--- model/node.go
+++ model/node.go
@@ -8,8 +8,8 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
-	"github.com/kolide/kolide/shared/hub"
-	"github.com/kolide/kolide/shared/osquery"
+	"github.com/JDSchmitz/kolide-archive/shared/hub"
+	"github.com/JDSchmitz/kolide-archive/shared/osquery"
 )
 
 // nodeUpdateStatus loop to keep node information current
